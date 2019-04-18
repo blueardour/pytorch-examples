f7b0059f466200dc0727ae95758ba029bb682eca
archlab <arclab@example.com>
2019-04-18 10:28:20 +0800

    fix bugs

--- imagenet/main.py
+++ imagenet/main.py
@@ -278,7 +278,8 @@ def main_worker(gpu, ngpus_per_node, args):
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        utils.adjust_learning_rate(optimizer, epoch, args)
+        lr = utils.adjust_learning_rate(optimizer, epoch, args)
+        logging.info('learning rate %f at epoch %d' %(lr, epcoh))
 
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
