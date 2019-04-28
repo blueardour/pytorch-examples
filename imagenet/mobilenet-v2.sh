81c717e64a0cd7a9a0e3430716be487fd0a191a1
phoenix <blueardour@phoenix>
2019-04-29 09:05:14 +0930

    resume training

--- imagenet/mobilenet-v2.sh
+++ imagenet/mobilenet-v2.sh
@@ -7,5 +7,6 @@ cd $FASTDIR/git/pytorch-examples/imagenet
 python main.py --data $FASTDIR/data/imagenet/ --arch 'mobilenet-v2' \
   --epochs 120 \
   --lr 0.045 --lr_policy 'decay' --lr_decay 0.95 --wd 4e-5 --nesterov \
-  --case 'batch256-nesterov'
+  --case 'batch256-nesterov' \
+  -r
 cd -
--- imagenet/resnet-2.sh
+++ imagenet/resnet-2.sh
@@ -5,6 +5,8 @@ fi
 
 cd $FASTDIR/git/pytorch-examples/imagenet
 python main.py --data $FASTDIR/data/imagenet/ --arch resnet18 \
+  --epochs 90 \
   --lr_fix_step 10 --lr_decay 0.3 --nesterov \
-  --case 'batch256-nesterov'
+  --case 'batch256-nesterov' \
+  -r
 cd -
